7627e612f0c0549c32145543697466ea46b84d14
André Garcia Marques <gmarques.andre@hotmail.com>
2022-05-01 14:33:59 -0300

    add context

--- VxTel.Api/VxTelDbContext.cs
+++ VxTel.Api/VxTelDbContext.cs
@@ -1,18 +1,47 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using VxTel.Api.Models;
+using VxTel.Shared.DefaultValues;
+using VxTel.Shared.Models;
 
 namespace VxTel.Api
 {
- 
-
     public class VxTelDbContext : DbContext
     {
-        public VxTelDbContext(DbContextOptions<VxTelDbContext> options) : base(options)
+        
+        public VxTelDbContext(DbContextOptions<VxTelDbContext> options)
+            : base(options)
         {
 
         }
+
+        public VxTelDbContext()
+        {
+
+        }
+
         public DbSet<CallPrice> CallPrices { get; set; }
         public DbSet<CallPlan> CallPlans { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+            var _defaultValues = new DefaultCallPrice();
+            modelBuilder.Entity<CallPrice>().HasData(_defaultValues.GetDefaultPrices());
+
+            var _defaultPlans = new DefaultCallPlan();
+            modelBuilder.Entity<CallPlan>().HasData(_defaultPlans.GetDefaultPlans());
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+            options.UseSqlServer(configuration.GetConnectionString("Default"));
+        }
 
     }
 }
