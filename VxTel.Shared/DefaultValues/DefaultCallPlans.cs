4fa2392e8271c0138a3b0f8f0ceb1824ace603f9
André Garcia Marques <gmarques.andre@hotmail.com>
2022-05-01 14:34:11 -0300

    add default values

---
+++ VxTel.Shared/DefaultValues/DefaultCallPlans.cs
@@ -0,0 +1,25 @@
+﻿using VxTel.Shared.Models;
+
+namespace VxTel.Shared.DefaultValues
+{
+    public class DefaultCallPlan
+    {
+        public DefaultCallPlan()
+        {
+
+        }
+
+        public List<CallPlan> GetDefaultPlans()
+        {
+            List<CallPlan> callPlanDefault = new()
+            {
+                new() { Id = 1, FreeTime = 30, Name = "FaleMais 30", ExcedeedTimeFeePercentage = 0.1, Price = 10 },
+                new() { Id = 2, FreeTime = 60, Name = "FaleMais 60", ExcedeedTimeFeePercentage = 0.1, Price = 20 },
+                new() { Id = 3, FreeTime = 120, Name = "FaleMais 120", ExcedeedTimeFeePercentage = 0.1, Price = 30 }
+            };
+
+            return callPlanDefault;
+        }
+
+    }
+}
---
+++ VxTel.Shared/DefaultValues/DefaultCallPrice.cs
@@ -0,0 +1,28 @@
+﻿using VxTel.Shared.Models;
+
+namespace VxTel.Shared.DefaultValues
+{
+    public class DefaultCallPrice
+    {
+        public DefaultCallPrice()
+        {
+
+        }
+
+        public List<CallPrice> GetDefaultPrices()
+        {
+            List<CallPrice> callPriceDefault = new()
+            {
+                new() { Id = 1, FromDDD = "011", ToDDD = "016", PricePerMinute = 1.90 },
+                new() { Id = 2, FromDDD = "016", ToDDD = "011", PricePerMinute = 2.90 },
+                new() { Id = 3, FromDDD = "011", ToDDD = "017", PricePerMinute = 1.70 },
+                new() { Id = 4, FromDDD = "017", ToDDD = "011", PricePerMinute = 2.70 },
+                new() { Id = 5, FromDDD = "011", ToDDD = "018", PricePerMinute = 0.90 },
+                new() { Id = 6, FromDDD = "018", ToDDD = "011", PricePerMinute = 1.90 },
+            };
+
+            return callPriceDefault;
+        }
+
+    }
+}
